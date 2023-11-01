c5dd23064e3c4761032882f681b06d3b1a1d8cbc
Inwoo Hwang <hsjkdss228@naver.com>
2023-11-02 02:05:57 +0900

    BugFix: Empty Constructor의 접근 제어 범위를 protected로 변경
    - private 정의 시 Entity 프록시 객체 생성 등
      JPA의 리플렉션을 통한 런타임 Entity 인스턴스 생성 시 예외가 발생하므로,
      리플렉션이 접근 가능하도록 protected로 접근 제어 범위 변경

--- src/main/java/com/wanted/babdoduk/user/domain/entity/User.java
+++ src/main/java/com/wanted/babdoduk/user/domain/entity/User.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseTimeEntity {
 
     @Id
