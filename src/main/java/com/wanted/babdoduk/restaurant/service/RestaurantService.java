c3a17f6020e99078e9523c86662551360ddc3002
buhee1029 <buhee1029@gmail.com>
2023-11-03 14:31:42 +0900

    Refactor: 코드리뷰 피드백 반영 2
    - 평가리스트 정보와 평점을 가져오는 부분을 각각 RestaurantReviewService, RestaurantReviewStatService 에 메서드로 분리
    - 서비스에서 다른 서비스 의존하지 않고 RestaurantController 컨트롤러에서 호출하여 반환하도록 변경

--- src/main/java/com/wanted/babdoduk/restaurant/service/RestaurantService.java
+++ src/main/java/com/wanted/babdoduk/restaurant/service/RestaurantService.java
@@ -2,13 +2,8 @@ package com.wanted.babdoduk.restaurant.service;
 
 import com.wanted.babdoduk.restaurant.domain.restaurant.Restaurant;
 import com.wanted.babdoduk.restaurant.domain.restaurant.RestaurantRepository;
-import com.wanted.babdoduk.restaurant.domain.review.repository.RestaurantReviewRepository;
-import com.wanted.babdoduk.restaurant.domain.review.repository.RestaurantReviewStatRepository;
-import com.wanted.babdoduk.restaurant.dto.RestaurantDetailResponseDto;
-import com.wanted.babdoduk.restaurant.dto.RestaurantReviewResponseDto;
 import com.wanted.babdoduk.restaurant.exception.ClosedRestaurantException;
 import com.wanted.babdoduk.restaurant.exception.NotFoundRestaurantException;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,21 +15,13 @@ public class RestaurantService {
     private static final String CLOSE_STATUS = "폐업";
 
     private final RestaurantRepository restaurantRepository;
-    private final RestaurantReviewRepository reviewRepository;
-    private final RestaurantReviewStatRepository statRepository;
 
     @Transactional(readOnly = true)
-    public RestaurantDetailResponseDto getRestaurant(Long id) {
+    public Restaurant getRestaurant(Long id) {
         Restaurant restaurant = findRestaurant(id);
         checkBusinessStatus(restaurant);
 
-        return RestaurantDetailResponseDto.of(
-                restaurant,
-                statRepository.findByRestaurantId(id).getAverageScore(),
-                reviewRepository.findAllByRestaurantIdOrderByCreatedAtDesc(id)
-                                .stream()
-                                .map(RestaurantReviewResponseDto::of)
-                                .collect(Collectors.toList()));
+        return restaurant;
     }
 
     private void checkBusinessStatus(Restaurant restaurant) {
