515da70f180302974be1230d31cecc57065e40e6
kmss6905 <kmss69052@naver.com>
2023-11-06 02:56:43 +0900

    Test: Open API 예외 응답 테스트

--- src/test/java/com/wanted/babdoduk/batch/client/OpenRestaurantClientTest.java
+++ src/test/java/com/wanted/babdoduk/batch/client/OpenRestaurantClientTest.java
@@ -2,8 +2,10 @@ package com.wanted.babdoduk.batch.client;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.wanted.babdoduk.batch.client.response.success.ClientRestaurant;
+import com.wanted.babdoduk.batch.exception.RestaurantClientException;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,5 +28,12 @@ class OpenRestaurantClientTest {
         // then
         assertThat(response).isNotNull();
         assertThat(response).hasSize(10);
+    }
+
+    @Test
+    @DisplayName("Open API Error 발생 시 예외를 던진다")
+    void throwExceptionWhenOccurClientError() {
+        assertThatThrownBy(() -> openRestaurantClient.execute(1, 10000))
+                .isInstanceOf(RestaurantClientException.class);
     }
 }
