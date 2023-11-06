1455e8bcd12288b916d650a0a50ecd4768ce19ef
buhee1029 <buhee1029@gmail.com>
2023-11-07 01:56:20 +0900

    Refactor: Enum SortType 캐싱

--- src/main/java/com/wanted/babdoduk/restaurant/domain/restaurant/enums/SortType.java
+++ src/main/java/com/wanted/babdoduk/restaurant/domain/restaurant/enums/SortType.java
@@ -1,7 +1,8 @@
 package com.wanted.babdoduk.restaurant.domain.restaurant.enums;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import java.util.stream.Stream;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -12,14 +13,19 @@ public enum SortType {
     DISTANCE("distance", "거리순"),
     RATING("rating", "평점순");
 
+    private static final Map<String, SortType> SORT_TYPE_MAP = new HashMap<>();
+
+    static {
+        for (SortType type : values()) {
+            SORT_TYPE_MAP.put(type.name, type);
+        }
+    }
+
     private final String name;
     private final String description;
 
     @JsonCreator
-    public static SortType of(String input) {
-        return Stream.of(SortType.values())
-                     .filter(type -> type.toString().equals(input.toUpperCase()))
-                     .findFirst()
-                     .orElse(null);
+    public static SortType of(String type) {
+        return SORT_TYPE_MAP.get(type);
     }
 }
