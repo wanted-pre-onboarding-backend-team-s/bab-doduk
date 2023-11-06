30003c92ed14ac2f77989790397c3ba6c616bfa8
min050410 <min050410@gmail.com>
2023-11-06 11:08:24 +0900

    Refactor: ApiResponse 리팩토링

--- src/main/java/com/wanted/babdoduk/common/response/ApiResponse.java
+++ src/main/java/com/wanted/babdoduk/common/response/ApiResponse.java
@@ -2,7 +2,6 @@ package com.wanted.babdoduk.common.response;
 
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 
 @Getter
 public class ApiResponse<T> {
@@ -10,28 +9,28 @@ public class ApiResponse<T> {
     private int status;
     private T data;
 
-    public ApiResponse() {
+    private ApiResponse() {
     }
 
-    public ApiResponse(int status) {
+    private ApiResponse(int status) {
         this.status = status;
     }
 
-    public ApiResponse(int status, T data) {
+    private ApiResponse(int status, T data) {
         this.status = status;
         this.data = data;
     }
 
     public static <T> ApiResponse<T> ok(T data) {
-        return new ApiResponse<T>(HttpStatus.OK.value(), data);
+        return new ApiResponse<>(HttpStatus.OK.value(), data);
     }
 
     public static <T> ApiResponse<T> created(T data) {
-        return new ApiResponse<T>(HttpStatus.CREATED.value(), data);
+        return new ApiResponse<>(HttpStatus.CREATED.value(), data);
     }
 
     public static <T> ApiResponse<T> noContent() {
-        return new ApiResponse<T>(HttpStatus.NO_CONTENT.value());
+        return new ApiResponse<>(HttpStatus.NO_CONTENT.value());
     }
 
 }
