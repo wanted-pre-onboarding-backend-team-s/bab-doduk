a7905329f6124ba5be5ed037c19536e663e9b668
Inwoo Hwang <hsjkdss228@naver.com>
2023-11-08 03:19:32 +0900

    Refactor: package-private 생성자를 Lombok @RequiredArgsConstructor로 수정

--- src/main/java/com/wanted/babdoduk/common/domain/enums/RequestUnauthenticated.java
+++ src/main/java/com/wanted/babdoduk/common/domain/enums/RequestUnauthenticated.java
@@ -3,8 +3,11 @@ package com.wanted.babdoduk.common.domain.enums;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpMethod;
 
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 public enum RequestUnauthenticated {
     PostUsers(HttpMethod.POST.name(), "/api/v1/users"),
     PostSessions(HttpMethod.POST.name(), "/api/v1/sessions");
@@ -15,11 +18,6 @@ public enum RequestUnauthenticated {
     private static final List<RequestUnauthenticated> REQUESTS_UNAUTHENTICATED
             = Arrays.stream(RequestUnauthenticated.values())
             .toList();
-
-    RequestUnauthenticated(String method, String path) {
-        this.method = method;
-        this.path = path;
-    }
 
     public static boolean contains(HttpServletRequest request) {
         return REQUESTS_UNAUTHENTICATED
