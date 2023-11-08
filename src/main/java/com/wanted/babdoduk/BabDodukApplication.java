2f8614fc00382065d992c70afb0427ba999b4a26
kmss6905 <kmss69052@naver.com>
2023-11-08 20:34:11 +0900

    Chore: 불필요한 애노테이션 제거

--- src/main/java/com/wanted/babdoduk/BabDodukApplication.java
+++ src/main/java/com/wanted/babdoduk/BabDodukApplication.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableFeignClients
 @EnableJpaAuditing
 @SpringBootApplication
-@RequiredArgsConstructor
 public class BabDodukApplication {
 
     public static void main(String[] args) {
