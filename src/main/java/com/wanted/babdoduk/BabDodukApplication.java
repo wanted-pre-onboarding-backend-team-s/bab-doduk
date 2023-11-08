ec6a2a0f1735e1064a50aac341584a46a8a4d529
min050410 <min050410@gmail.com>
2023-11-08 14:20:59 +0900

    Feature: 비동기 메소드 설정

--- src/main/java/com/wanted/babdoduk/BabDodukApplication.java
+++ src/main/java/com/wanted/babdoduk/BabDodukApplication.java
@@ -8,10 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableFeignClients
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableAsync
 public class BabDodukApplication implements CommandLineRunner {
 
     public static final int INIT_SIZE = 0;
